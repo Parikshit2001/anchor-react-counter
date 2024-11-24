6fea2f0edc7d945cb87673df14b948e40f26e2b2
Parikshit <parikshitgupta2001@gmail.com>
2024-11-24 10:21:31 +0530

    Add tailwind

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function App() {
-  return (
-    <div>
-      HI
-    </div>
-  )
+  return <div className="bg-black text-white text-center h-screen">HI</div>;
 }
 
-export default App
+export default App;
